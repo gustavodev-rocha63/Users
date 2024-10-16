33b1275c95051fd08858e9a83ffcab50d77288c5
Gustavo <gustavorocha8971p@gmail.com>
2024-10-16 20:06:17 -0300

    Update

--- src/main/java/com/example/Users/Users/UsersController.java
+++ src/main/java/com/example/Users/Users/UsersController.java
@@ -3,9 +3,8 @@ package com.example.Users.Users;
 import com.example.Users.Entity.Participants;
 import com.example.Users.Entity.ParticipantsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.stream.Collectors;
 import com.example.Users.Entity.ParticipantsRepository;
 import java.util.List;
@@ -20,5 +19,10 @@ public class UsersController {
     @GetMapping("/list")
     public List<Participants>Users(){
         return participantsRepository.findAll();
+    }
+
+    @PostMapping("/Test")
+    public Participants Request(@RequestBody Participants participants){
+        return participantsRepository.save(participants);
     }
 }
