edf0070164f7eaf5a6ab9a4999489472530f2050
Gustavo <gustavorocha8971p@gmail.com>
2024-10-22 19:44:18 -0300

    Updates

--- src/main/java/com/example/Users/Entity/Participants.java
+++ src/main/java/com/example/Users/Entity/Participants.java
@@ -1,6 +1,8 @@
 package com.example.Users.Entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Users")
@@ -8,11 +10,22 @@ public class Participants {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "member")
     private String member;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(length = 900)
     private String image;
 
     public Long getId(){
@@ -53,5 +66,13 @@ public class Participants {
 
     public void setImage(String image){
         this.image = image;
+    }
+
+    public String getMember(){
+        return member;
+    }
+
+    public void setMember(String member){
+        this.member = member;
     }
 }
--- src/main/java/com/example/Users/Users/UsersController.java
+++ src/main/java/com/example/Users/Users/UsersController.java
@@ -2,11 +2,13 @@ package com.example.Users.Users;
 
 import com.example.Users.Entity.Participants;
 import com.example.Users.Entity.ParticipantsRepository;
+import jakarta.persistence.Id;
+import jakarta.validation.Valid;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.stream.Collectors;
-import com.example.Users.Entity.ParticipantsRepository;
 import java.util.List;
 
 @RestController
@@ -16,13 +18,13 @@ public class UsersController {
     @Autowired
     private ParticipantsRepository participantsRepository;
 
-    @GetMapping("/list")
+    @GetMapping("/{id}")
     public List<Participants>Users(){
         return participantsRepository.findAll();
     }
 
-    @PostMapping("/Test")
-    public Participants Request(@RequestBody Participants participants){
-        return participantsRepository.save(participants);
+    @PostMapping("/server/")
+    public Participants Request(@Valid @RequestBody Participants participants){
+        return participantsRepository.save(participants );
     }
 }
